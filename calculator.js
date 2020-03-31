4838541a2d65d0a2db4139155a65bd60ccc9fb01
Jessica Liang <jessicursliang@gmail.com>
2020-03-30 21:05:56 -0700

    added method for holding calculator actions

--- calculator.js
+++ calculator.js
@@ -3,3 +3,11 @@ const exampleAdditionInput = {
   num2: 5,
   operation: 'add',
 }
+
+function calculator(input) {
+  const check_validity = checkInput(input);
+
+  if (check_validity) {
+    doCalculations(input);
+  }
+}
